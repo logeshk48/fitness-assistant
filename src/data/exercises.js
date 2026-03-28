fac85269a7802681f3f8a827f735873b96d10a8c
Logesh K <logeshlogesh093@gmail.com>
2026-03-28 14:11:12 +0530

    app.jsx updated

---
+++ src/data/exercises.js
@@ -0,0 +1,62 @@
+export const exerciseList = [
+  {
+    name: "Pushups",
+    type: "Strength",
+    muscles: ["chest", "triceps", "shoulders"],
+    variations: ["Standard", "Incline", "Decline", "Diamond"]
+  },
+  {
+    name: "Squats",
+    type: "Strength",
+    muscles: ["quads", "glutes", "hamstrings"],
+    variations: ["Bodyweight", "Jump Squat", "Goblet Squat"]
+  },
+  {
+    name: "Pullups",
+    type: "Strength",
+    muscles: ["lats", "biceps", "forearms"],
+    variations: ["Wide Grip", "Chin-up", "Neutral Grip"]
+  },
+  {
+    name: "Lunges",
+    type: "Strength",
+    muscles: ["quads", "glutes"],
+    variations: ["Forward", "Reverse", "Walking"]
+  },
+  {
+    name: "Plank",
+    type: "Core",
+    muscles: ["abs", "core", "shoulders"],
+    variations: ["Standard", "Side Plank", "Plank Reach"]
+  },
+  {
+    name: "Shoulder Press",
+    type: "Strength",
+    muscles: ["shoulders", "triceps"],
+    variations: ["Dumbbell", "Barbell", "Arnold Press"]
+  },
+  {
+    name: "Bicep Curls",
+    type: "Strength",
+    muscles: ["biceps"],
+    variations: ["Dumbbell", "Barbell", "Hammer Curl"]
+  },
+  {
+    name: "Tricep Dips",
+    type: "Strength",
+    muscles: ["triceps", "shoulders"],
+    variations: ["Bench Dips", "Parallel Bar Dips"]
+  },
+  {
+    name: "Deadlift",
+    type: "Strength",
+    muscles: ["back", "glutes", "hamstrings"],
+    variations: ["Conventional", "Sumo", "Romanian"]
+  },
+  {
+    name: "Running",
+    type: "Cardio",
+    muscles: ["legs", "calves"],
+    variations: ["Jogging", "Sprinting", "Treadmill"]
+  }
+];
